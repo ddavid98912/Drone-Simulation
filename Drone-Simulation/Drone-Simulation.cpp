4a7dfae0cfd67b646add5571c58de412ff18e5c4
Vlad Zorlescu <vlad.zorlescu@gmail.com>
2019-10-31 15:28:52 +0200

    Am adaugat globals.h si globals.cpp pt variabilele globale ( height, width, etc..)

--- Drone-Simulation/Drone-Simulation.cpp
+++ Drone-Simulation/Drone-Simulation.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include "globals.h"
 #include "Drone3D.h"
 
 int main()
 {
 	Drone3D d3d;
 	d3d.initWindow(800, 900, "Drone Simulation");
+	std::cout << mass << std::endl;
 	while (!d3d.shouldClose()) {
 		d3d.updateView();
 	}
---
+++ Drone-Simulation/globals.cpp
@@ -0,0 +1,5 @@
+#include "globals.h" 
+int mass = 10;
+int height = 1;
+int length = 10;
+int width = 2;
